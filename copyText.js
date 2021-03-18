9326c152dfeed3b80af5172e4c4ef25df4a8a253
ShKlinkenberg <s.klinkenberg@gmail.com>
2021-03-18 14:57:13 +0100

    copy path added

---
+++ copyText.js
@@ -0,0 +1,8 @@
+function copy_data(containerid) {
+  var range = document.createRange();
+  range.selectNode(containerid); //changed here
+  window.getSelection().removeAllRanges(); 
+  window.getSelection().addRange(range); 
+  document.execCommand("copy");
+  window.getSelection().removeAllRanges();
+}
