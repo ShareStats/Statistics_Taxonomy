e406115fdf5d8994c628c67f030e49075a6140af
Oliver Lindemann <lindemann@cognitive-psychology.eu>
2021-03-26 15:36:30 +0100

    tags in json taxonomy

--- create_json_taxonomy.R
+++ create_json_taxonomy.R
@@ -5,26 +5,47 @@ library(readxl)
 library(data.tree)
 library(rjson)
 
-get_taxonomie <- function(xlsx_file) {
-    tax_levels <- read_excel(xlsx_file, sheet = 1) %>%
-                  pivot_longer( cols = starts_with("Level ")) %>%
-                  mutate(level = as.numeric(str_replace_all(name, "Level ", ""))) %>%
-                  select(level, value) %>%
-                  drop_na()
-    n_levels = max(tax_levels$level)          
+process_levels <- function(data, label="Taxonomy") {
+    "data frame columns: levels and values"
+    n_levels = max(data$level)          
     
-    levels = c(Node$new("Taxonomie"), rep(NA, n_levels))
-    for (r in 1:nrow(tax_levels)) {
-      l = tax_levels[r,]$level
-      levels[[l+1]] <- levels[[l]]$AddChild(tax_levels[r, ]$value)
+    levels = c(Node$new(label), rep(NA, n_levels))
+    for (r in 1:nrow(data)) {
+      l = data[r,]$level
+      levels[[l+1]] <- levels[[l]]$AddChild(data[r, ]$value)
     }             
     
     return(levels[[1]])
 }
 
 
-tax = get_taxonomie("source/Taxonomie ShareStats versie 17 maart 2021.xlsx")
-tax_list = ToListExplicit(tax, unname = TRUE, 
-                               nameName = "name", childrenName = "sublevels")$sublevels
-                  
-write(toJSON(tax_list, indent=2), file="taxonomie.json")
+file = "source/Taxonomie ShareStats versie 17 maart 2021.xlsx"
+
+# TAXONOMY
+stats_df <- read_excel(file, sheet = 1) %>%
+            pivot_longer( cols = starts_with("Level ")) %>%
+            mutate(level = as.numeric(str_replace_all(name, "Level ", ""))) %>%
+            select(level, value) %>%
+            drop_na()
+
+# TAGS
+tags_df <- read_excel(file, sheet = 2) %>%
+            rename("1" = "Type of question", "2" = "...2") %>%
+            pivot_longer( cols = c("1", "2")) %>%
+            mutate(level = as.numeric(name)) %>%
+            select(level, value) %>%
+            drop_na()
+
+
+# make taxonomy 
+stats = process_levels(stats_df, label="Statistics Taxonomy")
+tags = process_levels(tags_df, label="Tags")
+
+taxonomy = Node$new("base")
+taxonomy$AddChildNode(stats)
+taxonomy$AddChildNode(tags)
+
+# export to single JSON file
+tmp = ToListExplicit(taxonomy, unname = TRUE, 
+                          nameName = "name", childrenName = "sublevels")$sublevels
+write(toJSON(tmp, indent=2), file="taxonomy.json")
